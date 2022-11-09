5720e9acb045f6be9e0e26c299d9c629731632d1
Naitik Shah <n@daaku.org>
2022-11-09 15:28:32 +0400

    add a test

--- js/tests/parser.mjs
+++ js/tests/parser.mjs
@@ -1,10 +1,46 @@
+import { tokens } from '../src/parser.mjs'
 import { test } from 'uvu'
 import * as assert from 'uvu/assert'
 
-test('Math.sqrt()', () => {
-  assert.is(Math.sqrt(4), 2)
-  assert.is(Math.sqrt(144), 12)
-  assert.is(Math.sqrt(2), Math.SQRT2)
+test('tokens', () => {
+  assert.equal(
+    [...tokens('(+ 1 42)')],
+    [
+      {
+        kind: '(',
+        offset: 0,
+        line: 0,
+        column: 0,
+      },
+      {
+        kind: 'symbol',
+        value: '+',
+        offset: 1,
+        line: 0,
+        column: 1,
+      },
+      {
+        kind: 'symbol',
+        value: '1',
+        offset: 3,
+        line: 0,
+        column: 2,
+      },
+      {
+        kind: 'symbol',
+        value: '42',
+        offset: 5,
+        line: 0,
+        column: 3,
+      },
+      {
+        kind: ')',
+        offset: 7,
+        line: 0,
+        column: 3,
+      },
+    ],
+  )
 })
 
 test.run()
