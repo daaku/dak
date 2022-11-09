2eead6c9f3aaa08922eb76cda422304aa8420ea8
Naitik Shah <n@daaku.org>
2022-11-09 08:52:08 +0400

    more fennel like function declarations

--- app.clj
+++ app.clj
@@ -7,19 +7,19 @@
         ["./pie.js" :refer [Pie]])
 
 ; Janky title case.
-(defn- title-case [s]
+(fn title-case [s]
   (-> s
       (.split " ")
       (.map #(%.replace (. % 0) (.toUpperCase (. % 0))))
       (.join " ")))
 
-(defn- today-path
+(fn today-path
   "Make the path for today's plan."
   []
   (let [d (Date.)]
     (str "#/date/" (d.getFullYear) "/" (inc (d.getMonth)) "/" (d.getDate))))
 
-(defn- site-header
+(fn site-header
   "Render the top level site header."
   [ctx & children]
   #[:header
@@ -29,12 +29,12 @@
            [:li [:a {:href "#/about"} "About"]]
            [:li [:a {:href (today-path)} "Today"]]]]])
 
-(defn- render-quantity [q]
+(fn render-quantity [q]
   (let [qs (quantity-string q)]
     (when-not (= qs "")
       #[:span qs])))
 
-(defn- render-pie [summary]
+(fn render-pie [summary]
   (let [p (Pie. {:data [{:label "Protein"
                          :amount (or summary.macros?.protein?.amount 0)}
                         {:label "Fat"
@@ -51,13 +51,13 @@
                         :dominant-baseline "middle" :text-anchor "middle"}
                  title]]))]))
 
-(defn- render-missing [missing]
+(fn render-missing [missing]
   (when (pos? missing.length)
     #[:<>
       [:h4 "Missing Nutrition Data"]
       [:ul (missing.map #(#[:li %]))]]))
 
-(defn- render-date [{:keys [ctx year month date]}]
+(fn render-date [{:keys [ctx year month date]}]
   (let [date (Date. (+ year) (- month 1) (+ date))
         recipe (ctx.db.findDay date)
         summary (summarizeMacros ctx.db recipe)]
@@ -71,37 +71,37 @@
        [:div [render-macros summary]
         [render-pie summary]]]]))
 
-(defn- render-macros [summary]
-  (let [row (fn [label macro indent]
-              (let [q (?. summary.macros (. Macro macro))]
-                (if (pos? q?.amount)
-                  #[:tr
-                    [:th (if indent [:span label] label)]
-                    [:td (quantity-string q)]])))]
-    #[:table [:tbody
-              [row "Calorie" :Calorie false]
-              [row "Fat" :Fat false]
-              [row "Saturated Fat" :SaturatedFat true]
-              [row "Carb" :Carb false]
-              [row "Fiber" :Fiber true]
-              [row "Sugar" :Sugar true]
-              [row "Protein" :Protein false]
-              [row "Salt" :Salt false]
-              [row "Potassium" :Potassium false]]]))
+(fn render-macros [summary]
+  (fn row [label macro indent]
+    (let [q (?. summary.macros (. Macro macro))]
+      (if (pos? q?.amount)
+        #[:tr
+          [:th (if indent [:span label] label)]
+          [:td (quantity-string q)]])))
+  #[:table [:tbody
+            [row "Calorie" :Calorie false]
+            [row "Fat" :Fat false]
+            [row "Saturated Fat" :SaturatedFat true]
+            [row "Carb" :Carb false]
+            [row "Fiber" :Fiber true]
+            [row "Sugar" :Sugar true]
+            [row "Protein" :Protein false]
+            [row "Salt" :Salt false]
+            [row "Potassium" :Potassium false]]])
 
-(defn- auth-status [ctx]
+(fn auth-status [ctx]
   (if-let [email ctx?.auth?.user?.email]
     #[:p "Welcome " [:strong email]]
     #[:p "Sign in for the best experience."]))
 
-(defn- home [ctx]
+(fn home [ctx]
   #[:<>
     [site-header ctx "Home"]
     [auth-status ctx]
     [:a {:href "#/about"} "About"]
     [:a {:href (today-path)} "Today"]])
 
-(defn- about [ctx]
+(fn about [ctx]
   (let [ref {}
         count 0
         onclick (fn []
@@ -113,11 +113,11 @@
       [:div [:button {:onclick onclick} "Change Me"]]
       [:div "Tell us more."]]))
 
-(defn- make-handler [component ctx]
+(fn make-handler [component ctx]
   (let [root (document.getElementById "root")]
     #(root.replaceChildren #[component ctx %])))
 
-(defn@ main [fs]
+(fn@ ^:export main [fs]
   (let [db (DB. fs "../data/")
         auth @(Auth.new {:apiKey __FIREBASE_API_KEY__})
         router (Router.)
