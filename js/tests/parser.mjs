2d7337f54b7e019246cfbbab4e4d5389b213631f
Naitik Shah <n@daaku.org>
2022-11-09 15:20:41 +0400

    some more code

---
+++ js/tests/parser.mjs
@@ -0,0 +1,10 @@
+import { test } from 'uvu';
+import * as assert from 'uvu/assert';
+
+test('Math.sqrt()', () => {
+  assert.is(Math.sqrt(4), 2);
+  assert.is(Math.sqrt(144), 12);
+  assert.is(Math.sqrt(2), Math.SQRT2);
+});
+
+test.run();
