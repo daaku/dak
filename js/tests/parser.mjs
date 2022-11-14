bd189b1f4795c6282ac49a17565b5b308f81d499
Naitik Shah <n@daaku.org>
2022-11-14 20:27:03 +0530

    use table style for tests

--- js/tests/parser.mjs
+++ js/tests/parser.mjs
@@ -16,71 +16,74 @@ const tostr = code => {
   return pieces.join('')
 }
 
-test('transpile', () => {
-  assert.equal(tostr(':foo'), '"foo"')
-  assert.equal(
-    tostr('{:a 1 :b [1 2] :c [3 4]}'),
-    '{["a"]:1,["b"]:[1,2,],["c"]:[3,4,],}',
-  )
-  assert.equal(tostr('[[1 2 3] [4 5 6]]'), '[[1,2,3,],[4,5,6,],]')
-  assert.equal(tostr('(a {:b c})'), 'a({["b"]:c,},)')
-  assert.equal(tostr('(.a b {:c d})'), 'b.a({["c"]:d,},)')
-  assert.equal(
-    tostr(`
+const cases = [
+  [':foo', '"foo"'],
+  ['{:a 1 :b [1 2] :c [3 4]}', '{["a"]:1,["b"]:[1,2,],["c"]:[3,4,],}'],
+  ['[[1 2 3] [4 5 6]]', '[[1,2,3,],[4,5,6,],]'],
+  ['(a {:b c})', 'a({["b"]:c,},)'],
+  ['(.a b {:c d})', 'b.a({["c"]:d,},)'],
+  [
+    `
     (import ["./a.js" [A B c-d]]
             ["./b/c.js" [E]])
-    `),
+    `,
     `import {A,B,c-d,} from "./a.js";import {E,} from "./b/c.js";`,
-  )
-  assert.equal(tostr('(def a 42)'), 'let a=42;')
-  assert.equal(
-    tostr(`
+  ],
+  ['(def a 42)', 'let a=42;'],
+  [
+    `
     (fn err [expected offset]
       (str "expected " expected " at position " offset))
-    `),
+    `,
     `const err=(expected,offset,)=>{return "expected "+expected+" at position "+offset;}`,
-  )
-  assert.equal(
-    tostr(`
+  ],
+  [
+    `
     (fn first [[first second] other]
       first)
-    `),
+    `,
     `const first=([first,second,],other,)=>{return first;}`,
-  )
-  assert.equal(
-    tostr(`
+  ],
+  [
+    `
     (fn run [{x :y :keys [b c] :or {a 1 b 2 d 3}}]
       a)
-    `),
+    `,
     `const run=({y:x,b=2,c,a=1,d=3},)=>{return a;}`,
-  )
-  assert.equal(
-    tostr(`
+  ],
+  [
+    `
     (fn run []
       (let [a 0
             b (inc a)]
         (console.log a)
         b))
-    `),
+    `,
     `const run=()=>{return (() => {let a=0,b=inc(a,);console.log(a,);return b;})();}`,
-  )
-  assert.equal(tostr('(throw (error "foo"))'), 'throw error("foo",);')
-  assert.equal(
-    tostr(`
+  ],
+  ['(throw (error "foo"))', 'throw error("foo",);'],
+  [
+    `
     (for [i 0 len step]
       (console.log i))
-    `),
+    `,
     `for(let i=0;i<len;i+=step){console.log(i,);}`,
-  )
-  assert.equal(
-    tostr(`
+  ],
+  [
+    `
     (case (inc 1)
       "foo" :bar
       "baz" :boo
       :otherwise)
-    `),
+    `,
     `switch (inc(1,)){case "foo":"bar";break;case "baz":"boo";break;default:"otherwise";break}`,
-  )
+  ],
+]
+
+test('transpile', () => {
+  cases.forEach(([input, output]) => {
+    assert.equal(tostr(input), output)
+  })
 })
 
 test.run()
