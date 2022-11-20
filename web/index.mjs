44d8b6a8687095bc45070e696b87f15f49b113af
Naitik Shah <n@daaku.org>
2022-11-20 15:00:25 +0530

    dak playground: show errors, println, options

--- web/index.mjs
+++ web/index.mjs
@@ -2,17 +2,64 @@ import prettier from "https://unpkg.com/prettier@2.7.1/esm/standalone.mjs";
 import parserBabel from "https://unpkg.com/prettier@2.7.1/esm/parser-babel.mjs";
 import { transpile } from "../js/src/parser.mjs";
 
-const input = document.getElementById("input");
+const autoEval = document.getElementById("autoEval");
+const pretty = document.getElementById("pretty");
+const minify = document.getElementById("minify");
+const dakCode = document.getElementById("dakCode");
+const jsCode = document.getElementById("jsCode");
 const output = document.getElementById("output");
-input.oninput = (ev) => {
+
+const isPrimitive = (v) => v === Object(v);
+
+globalThis.println = (...rest) => {
+  console.log(...rest);
+  for (const thing of rest) {
+    const child = document.createElement("pre");
+    if (isPrimitive(thing)) {
+      child.innerText = String(thing);
+    } else {
+      child.innerText = JSON.stringify(thing);
+    }
+    output.appendChild(child);
+  }
+};
+
+const logErr = (prefix, err) => {
+  console.error(err);
+  const child = document.createElement("pre");
+  child.innerText = prefix + ":" + err;
+  output.appendChild(child);
+};
+
+dakCode.oninput = (ev) => {
+  output.replaceChildren();
+
   try {
-    const js = [...transpile(input.value)].join("");
-    const fmt = prettier.format(js, {
-      parser: "babel",
-      plugins: [parserBabel],
-    });
-    output.value = fmt;
+    const js = [...transpile(dakCode.value)].join("");
+
+    if (autoEval.value) {
+      try {
+        eval(js);
+      } catch (e) {
+        logErr("eval", e);
+      }
+    }
+
+    if (pretty.value) {
+      try {
+        jsCode.value = prettier.format(js, {
+          parser: "babel",
+          plugins: [parserBabel],
+        });
+        return;
+      } catch (e) {
+        logErr("pretty", e);
+      }
+    }
+
+    // if we get here we're not doing pretty or minify
+    jsCode.value = js;
   } catch (e) {
-    console.error(e);
+    logErr("compile", e);
   }
 };
