273c38a9f4d728ae1f8badf41eb3238c9e3017c7
Naitik Shah <n@daaku.org>
2022-12-05 18:43:24 +0400

    initial cli

---
+++ packages/cli/main.mjs
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+import { transpile } from '@daklang/transpiler'
+import { readFileSync } from 'node:fs'
+import process from 'node:process'
+
+const all = []
+const input = readFileSync(0, 'utf-8')
+const stdout = process.stdout
+for (const output of transpile(input)) {
+  all.push(stdout.write(output, 'utf-8'))
+}
+all.push(stdout.write('\n', 'utf-8'))
+await Promise.all(all)
