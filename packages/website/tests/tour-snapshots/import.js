f0730307c1015cc2c30e77cc93d349e4d71f5109
Naitik Shah <n@daaku.org>
2023-02-17 12:09:37 +0200

    website: test tour output

---
+++ packages/website/tests/tour-snapshots/import.js
@@ -0,0 +1,24 @@
+
+
+
+
+import {readFile,writeFile} from "node:fs"
+
+
+import {readFile} from "node:fs"
+import {dirname} from "node:path"
+
+
+import Button from "./button.js"
+
+
+import * as ui from "./ui.js"
+
+
+import {Button as MainButton,Label as MainLabel} from "./main/ui.js"
+import {Button as AltButton} from "./alt/ui.js"
+
+
+import TheDefault,{Label,Button as MainButton} from "./main/ui.js"
+import {readFile} from "node:fs"
+
---
+++ packages/website/tests/tour-snapshots/variables.js
@@ -0,0 +1,31 @@
+
+var answer=42
+const name="deep thought"
+
+let machine={planet:"earth",}
+prn(answer,name,machine.planet)
+
+answer=43
+machine.planet="destroyed"
+prn(answer,name,machine.planet)
+
+
+prop_DASH_name="planet"
+prn(machine[prop_DASH_name])
+machine[prop_DASH_name]="destroyed"
+
+{let author="Adams"
+let programmer="Lunkwill"
+prn(author,programmer)
+}
+
+
+
+
+
+{let is_DASH_true_QMARK_=true
+let bad_BANG_=false
+let in_GT_=0
+let out_LT_=1
+prn(is_DASH_true_QMARK_,bad_BANG_,in_GT_,out_LT_)
+}
