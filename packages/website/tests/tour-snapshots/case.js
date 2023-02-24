852d0dcc31a29cdf3246db2937326abc989f8073
Naitik Shah <n@daaku.org>
2023-02-24 19:36:23 +0400

    tour: case
    closes #81

---
+++ packages/website/tests/tour-snapshots/case.js
@@ -0,0 +1,30 @@
+
+
+
+
+
+for(let i=40
+i<43
+i++){switch (i){case 41:prn(i,"warm")
+break
+case 42:prn(i,"boom")
+break
+default:prn(i,"no dice")
+break}
+}
+
+const answer=42
+
+let hoist__0
+switch (answer){case 42:hoist__0="boom"
+break
+default:hoist__0="no dice"
+break}
+prn("hoisted",hoist__0)
+
+const run=(answer)=>{switch (answer){case 41:return "warm"
+case 42:return "boom"
+default:return "no dice"
+}
+}
+prn("run returns",run(42))
