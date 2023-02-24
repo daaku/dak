035137e98dad69d19dce3084e24a6633e350fc36
Naitik Shah <n@daaku.org>
2023-02-24 18:38:02 +0400

    tour: loops
    closes #80

---
+++ packages/website/tests/tour-snapshots/loops.js
@@ -0,0 +1,35 @@
+
+{let i=5
+while(i>0){prn("while:",(i--))
+}
+}
+
+for(let i=0
+i<5
+i++){prn("for step=1",i)
+}
+
+for(let i=0
+i<10
+i+=2){prn("for step=2",i)
+}
+
+const vs=["a","b","c",]
+
+for(let v of vs){prn("for-of:",v)
+}
+
+const os=[{name:"yoda",age:900,},{name:"luke",age:90,},]
+
+for(let {name,age} of os){prn(name,"is",age,"years old")
+}
+
+const yoda={name:"yoda",age:900,}
+
+for(let p in yoda){prn(p,yoda[p])
+}
+
+const ps=[Promise.resolve(1),Promise.resolve(2),]
+
+for await(let v of ps){prn("for@",v)
+}
