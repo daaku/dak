63b1f39d846ac67e9879210e06fc56daad0f859e
Naitik Shah <n@daaku.org>
2023-02-24 18:21:38 +0400

    tour: class
    closes #77

---
+++ packages/website/tests/tour-snapshots/classes.js
@@ -0,0 +1,27 @@
+
+
+class Animal{}
+
+class Dinosaur extends Animal{}
+
+const TRex=class extends Dinosaur{}
+
+
+
+const pet=new TRex()
+
+
+prn("instanceof TRex =",pet instanceof TRex)
+prn("isa? Dinosaur =",pet instanceof Dinosaur)
+
+
+class Stego extends Animal{#name
+constructor(name){super()
+
+this.#name=name
+
+
+return
+}async greet(){return prn(this.#name,"says hello.")
+}}
+await new Stego("Barney").greet()
