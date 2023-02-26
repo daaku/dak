cab0e373614e9ec6fa6b3eab20053e488435ef8e
Naitik Shah <n@daaku.org>
2023-02-26 17:07:07 +0400

    tour: async
    closes #84

---
+++ packages/website/tests/tour-snapshots/async.js
@@ -0,0 +1,12 @@
+
+const hello=async(w)=>{return prn("hello",w)
+}
+
+await hello("world")
+
+await hello("earth")
+
+const ps=[Promise.resolve(1),Promise.resolve(2),]
+
+for await(let v of ps){prn("for@",v)
+}
